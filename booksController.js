edb55055f721da78ec534ac88a512bd7ddb2c71f
AndrewWilkinson <AndrewWilkinson@users.noreply.github.com>
2021-10-26 08:56:30 +0100

    API router & controller, no testing

---
+++ booksController.js
@@ -0,0 +1,121 @@
+let books = []
+
+let exampleBook =
+{
+    id: -1,
+    title: "Untitled",
+    author: "Unknown",
+    available: 3,
+    read: ["Andrew", "Bob"]
+}
+
+books.push(exampleBook);
+let IdNumber = 0
+const createError = require('http-errors')
+
+exports.index = function (req, res) {
+    res.send(books)
+}
+
+exports.create = function (req, res, next) {
+    if (!req.body.title) {
+        return (next(createError(400, "Title is requred")))
+    }
+    else if (!req.body.author) {
+        return (next(createError(400, "Author is requred")))
+    }
+    books.push({ id: IdNumber, title: req.body.title, author: req.body.author, read: false })
+    IdNumber++
+    res.send({ result: true })
+}
+
+exports.show = function (req, res, next) {
+    const foundBook = books.find((book) => book.id == req.params.id)
+    if (!foundBook) {
+        return (next(createError(404, "No book of that Id can be found")))
+    }
+    res.send(foundBook)
+}
+
+exports.deleteByID = function (req, res, next) {
+    const foundBook = books.find((book) => book.id == req.params.id)
+    if (!foundBook) {
+        return (next(createError(404, "No book of that Id found")))
+    }
+    books = books.filter((book) => book.id != req.params.id)
+    res.send(`${foundBook.id} deleted`)
+}
+
+exports.edit = function (req, res, next) {
+
+    if (!req.body.name) {
+        return (next(createError(400, "Title is requred")))
+    }
+    else if (!req.body.author) {
+        return (next(createError(400, "Author is requred")))
+    }
+
+    const bookFound = books.find((book) => book.id == req.params.id)
+    {
+        if (!bookFound) {
+            return (next(createError(404, "No book of that Id found")))
+        }
+
+        books = books.map((book) => {
+            if (book.id == req.params.id) {
+                book.title = req.body.title;
+                book.author = req.body.author;
+            }
+            return book
+        })
+        res.send({ result: true })
+    }
+}
+
+exports.checkout = function (req, res, next) {
+
+    const bookFound = books.find((book) => book.id == req.params.id)
+    {
+        if (!bookFound) {
+            return (next(createError(404, "No book of that Id found")))
+        }
+
+        if (!bookFound.available) {
+            bookFound.available--;/////////////Needs to happen on a persisted object via a persisting mutation
+            res.send(`One copy of ${bookFound.title} has been checked out`)
+        }
+        else {
+            return (next(createError(404, "No copies of that book are available")))
+        }
+
+    }
+}
+
+exports.return = function (req, res, next) {
+
+    const bookFound = books.find((book) => book.id == req.params.id)
+    {
+        if (!bookFound) {
+            return (next(createError(404, "No book of that Id found")))
+        }
+        bookFound.available++;/////////////Needs to happen on a persisted object via a persisting mutation
+        res.send(`One copy of ${bookFound.title} has been returned`)
+    }
+
+}
+
+exports.read = function (req, res, next) {
+
+    if (!req.body.name) {
+        return (next(createError(400, "A name is required")))
+    }
+
+    const bookFound = books.find((book) => book.id == req.params.id)
+    {
+        if (!bookFound) {
+            return (next(createError(404, "No book of that Id found")))
+        }
+        bookFound.read.push("req.params.id")/////////////Needs to happen on a persisted object via a persisting mutation
+        res.send(`${req.body.name} is now a proud reader of ${bookFound.title}`)
+    }
+}
